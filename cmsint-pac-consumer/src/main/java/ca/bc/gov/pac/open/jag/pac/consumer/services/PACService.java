84dbae8baf025b7d35fabf05e9fd03bfd45ff414
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2022-11-16 17:09:50 -0800

    Unit test for pollOrdsForNewRecords (not all of them passing)

--- cmsint-pac-consumer/src/main/java/ca/bc/gov/pac/open/jag/pac/consumer/services/PACService.java
+++ cmsint-pac-consumer/src/main/java/ca/bc/gov/pac/open/jag/pac/consumer/services/PACService.java
@@ -8,9 +8,6 @@ import ca.bc.gov.pac.open.jag.pac.consumer.model.EventTypeEnum;
 import ca.bc.gov.pac.open.jag.pac.consumer.model.SynchronizeClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,14 +23,14 @@ import org.springframework.ws.client.core.WebServiceTemplate;
 @Service
 @Slf4j
 public class PACService {
-    @Value("${icon.host}")
+    @Value("${pac.ords.host}")
     private String cmsHost = "https://127.0.0.1/";
-
-    @Value("${icon.cmsint-host}")
-    private String cmsintHost = "https://127.0.0.1/";
 
     @Value("${icon.pac-service-url}")
     private String pacServiceUrl = "https://127.0.0.1/";
+
+    @Value("${pac.ords.endpoints.success}")
+    private String ordsSuccessEndpoint;
 
     private final WebServiceTemplate webServiceTemplate;
     private final RestTemplate restTemplate;
@@ -50,58 +47,17 @@ public class PACService {
     // PACUpdate BPM
     public void processPAC(Client client) throws JsonProcessingException {
 
-        String eventType = getEventType(client);
-
-        Client eventClient = client.getCopyWithNewEventTypeCode(eventType);
-
-        Optional<Client> updatedClientOptional = pacUpdate(eventClient);
-
-        if (!updatedClientOptional.isPresent()) return;
-
-        Client updatedClient = updatedClientOptional.get();
-
-        SynchronizeClient synchronizeClient = composeSoapServiceRequestBody(updatedClient);
+        SynchronizeClient synchronizeClient = composeSoapServiceRequestBody(client);
 
         invokeSoapService(synchronizeClient);
 
-        pacSuccess(updatedClient);
+        pacSuccess(client);
         // End of BPM
-    }
-
-    private Optional<Client> pacUpdate(Client client) {
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(cmsHost + "pac/update");
-        try {
-            HttpEntity<Client> respClient =
-                    restTemplate.exchange(
-                            builder.toUriString(),
-                            HttpMethod.POST,
-                            new HttpEntity<>(client, new HttpHeaders()),
-                            new ParameterizedTypeReference<>() {});
-
-            log.info(new RequestSuccessLog("Request Success", "pacUpdate").toString());
-
-            if (respClient.getBody().getStatus().equals("0")) {
-                log.info("PAC update cancel");
-                return Optional.empty();
-            }
-            return Optional.ofNullable(respClient.getBody());
-        } catch (Exception ex) {
-
-            log.error(
-                    new OrdsErrorLog(
-                                    "Error received from ORDS",
-                                    "pacUpdate",
-                                    ex.getMessage(),
-                                    client)
-                            .toString());
-
-            throw new ORDSException();
-        }
     }
 
     private void pacSuccess(Client client, Client eventClient) throws JsonProcessingException {
         UriComponentsBuilder builder =
-                UriComponentsBuilder.fromHttpUrl(cmsHost + "pac/success")
+                UriComponentsBuilder.fromHttpUrl(cmsHost + ordsSuccessEndpoint)
                         .queryParam("clientNumber", client.getClientNumber())
                         .queryParam("eventSeqNum", client.getEventSeqNum())
                         .queryParam("computerSystemCd", client.getComputerSystemCd());
@@ -154,35 +110,9 @@ public class PACService {
         return synchronizeClient;
     }
 
-    private String getEventType(Client client) throws ORDSException, JsonProcessingException {
-        UriComponentsBuilder builder =
-                UriComponentsBuilder.fromHttpUrl(cmsintHost + "event-type")
-                        .queryParam("clientNumber", client.getClientNumber())
-                        .queryParam("eventSeqNum", client.getEventSeqNum());
-        try {
-            HttpEntity<Map<String, String>> resp =
-                    restTemplate.exchange(
-                            builder.toUriString(),
-                            HttpMethod.POST,
-                            new HttpEntity<>(new HttpHeaders()),
-                            new ParameterizedTypeReference<>() {});
-            log.info(new RequestSuccessLog("Request Success", "getEventType").toString());
-            return Objects.requireNonNull(resp.getBody()).get("eventTypeCode");
-        } catch (Exception ex) {
-            log.error(
-                    new OrdsErrorLog(
-                                    "Error received from ORDS",
-                                    "getEventType",
-                                    ex.getMessage(),
-                                    client)
-                            .toString());
-            throw new ORDSException();
-        }
-    }
-
     private HttpEntity<Client> sendUpdateRequest(Client client) {
         UriComponentsBuilder builder;
-        builder = UriComponentsBuilder.fromHttpUrl(cmsHost + "pac/update");
+        builder = UriComponentsBuilder.fromHttpUrl(cmsHost + ordsSuccessEndpoint);
         HttpEntity<Client> respClient =
                 restTemplate.exchange(
                         builder.toUriString(),
---
+++ cmsint-pac-consumer/src/test/java/ca/bc/gov/pac/open/jag/pac/consumer/services/PACServiceTest.java
@@ -0,0 +1,9 @@
+package ca.bc.gov.pac.open.jag.pac.consumer.services;
+
+import org.junit.jupiter.api.Test;
+
+class PACServiceTest {
+
+    @Test
+    void getEventType() {}
+}
