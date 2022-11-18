3956f4f929cbb6c0c47dd5240aaab211c41f1040
Gabriel Poleze Ferreira <gabriel.poleze@gmail.com>
2022-11-17 18:43:13 -0800

    fixing rabbitmq missing beans

--- cmsint-pac-poller/src/main/java/ca/bc/gov/open/jag/pac/poller/config/RabbitMqConfig.java
+++ cmsint-pac-poller/src/main/java/ca/bc/gov/open/jag/pac/poller/config/RabbitMqConfig.java
@@ -1,10 +1,13 @@
 package ca.bc.gov.open.jag.pac.poller.config;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +16,9 @@ import org.springframework.web.client.RestTemplate;
 public class RabbitMqConfig {
 
     private final QueueConfig queueConfig;
+
+    @Value("${spring.rabbitmq.host}")
+    private String rabbitHost;
 
     @Autowired
     public RabbitMqConfig(QueueConfig queueConfig) {
@@ -43,5 +49,16 @@ public class RabbitMqConfig {
     public Declarables binding(@Qualifier("pac-queue") Queue pacQueue, DirectExchange exchange) {
         return new Declarables(
                 BindingBuilder.bind(pacQueue).to(exchange).with(queueConfig.getPacRoutingkey()));
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory =
+                new CachingConnectionFactory(rabbitHost);
+        return connectionFactory;
+    }
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        return new RabbitTemplate(connectionFactory());
     }
 }
