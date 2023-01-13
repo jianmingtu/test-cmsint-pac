dbd5bb43b7066086f10696a00db98dcb836201d6
jianming tu <tu_jm@yahoo.com>
2023-01-13 13:54:16 -0800

    clean up

--- cmsint-pac-extractor/src/main/java/ca/bc/gov/open/jag/pac/extractor/config/RabbitMqConfig.java
+++ cmsint-pac-extractor/src/main/java/ca/bc/gov/open/jag/pac/extractor/config/RabbitMqConfig.java
@@ -4,6 +4,7 @@ import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -56,5 +57,10 @@ public class RabbitMqConfig {
         connectionFactory.setUsername(username);
         connectionFactory.setPassword(password);
         return connectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        return new RabbitTemplate(connectionFactory());
     }
 }
