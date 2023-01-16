e6fa7fb47428067e13d3c62b0985217b9a87e366
jianming tu <tu_jm@yahoo.com>
2023-01-16 15:00:21 -0800

    add username and password to ORDS

--- cmsint-pac-loader/src/main/java/ca/bc/gov/open/jag/pac/loader/config/TemplateConfig.java
+++ cmsint-pac-loader/src/main/java/ca/bc/gov/open/jag/pac/loader/config/TemplateConfig.java
@@ -8,8 +8,8 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import java.time.Instant;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -25,20 +25,9 @@ public class TemplateConfig {
     @Value("${ords.password}")
     private String password;
 
-    @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        var restTemplate = restTemplateBuilder.basicAuthentication(username, password).build();
         restTemplate.getMessageConverters().add(0, createMappingJacksonHttpMessageConverter());
-        restTemplate
-                .getInterceptors()
-                .add(
-                        (request, body, execution) -> {
-                            String auth = username + ":" + password;
-                            byte[] encodedAuth = Base64.encodeBase64(auth.getBytes());
-                            request.getHeaders()
-                                    .add("Authorization", "Basic " + new String(encodedAuth));
-                            return execution.execute(request, body);
-                        });
         return restTemplate;
     }
 
